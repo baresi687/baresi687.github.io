ad1aa39017ba8354839ea5e937ac9d14bb3f53b1
hreinn <hreinngy@gmail.com>
2023-06-25 18:56:26 +0200

    Adds SvelteKit ChatGPT project. Updates introduction text. Adds separate images for thumbnail and modal for improved image clarity.

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ if (localStorage.theme === "light") document.body.classList.add("light")
 
 screenShots.forEach((item) => {
   item.onclick = function () {
-    screenShotModalImg.src = this.src;
+    screenShotModalImg.src = this.src.slice(0, this.src.length - 4) + '-modal.jpg'
     screenShotModalImg.alt = this.alt;
     screenShotModal.classList.add("show")
   }
